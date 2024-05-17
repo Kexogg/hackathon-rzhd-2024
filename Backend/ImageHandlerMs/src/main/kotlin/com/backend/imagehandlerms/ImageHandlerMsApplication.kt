3b57ff5066eddfb2712dfd0462cc6dbb7aaaf0cc
Ilya <74655944+JabaXNT@users.noreply.github.com>
2024-05-17 22:01:59 +0500

    kotlin ms added

---
+++ Backend/ImageHandlerMs/src/main/kotlin/com/backend/imagehandlerms/ImageHandlerMsApplication.kt
@@ -0,0 +1,11 @@
+package com.backend.imagehandlerms
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImageHandlerMsApplication
+
+fun main(args: Array<String>) {
+    runApplication<ImageHandlerMsApplication>(*args)
+}
