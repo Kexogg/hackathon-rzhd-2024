8bf1d22df6fe8f1f46e952176e658ffa7d7b1163
Ilya <74655944+JabaXNT@users.noreply.github.com>
2024-05-18 12:15:36 +0500

    Mega Major fix

--- Backend/ImageHandlerMs/src/main/kotlin/com/backend/imagehandlerms/services/PythonMLConsumerService.kt
+++ Backend/ImageHandlerMs/src/main/kotlin/com/backend/imagehandlerms/services/PythonMLConsumerService.kt
@@ -9,12 +9,10 @@ class PythonMLConsumerService {
     @RabbitListener(queues = ["\${RABBITMQ_QUEUE}"])
     fun receiveMessage(message: String) {
         println("Received message: $message")
-        // process the message here
     }
 
     @RabbitListener(queues = ["\${PYTHON_SERVICE_QUEUE}"])
     fun receivePythonServiceMessage(message: String) {
         println("Received message from Python service: $message")
-        // process the message here
     }
 }
