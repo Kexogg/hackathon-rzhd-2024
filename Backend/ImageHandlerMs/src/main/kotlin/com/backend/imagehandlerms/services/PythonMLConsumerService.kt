775fa5469f4f978d6237cc8f8606d363b515a1cf
Ilya <74655944+JabaXNT@users.noreply.github.com>
2024-05-18 12:45:08 +0500

    commented pythonConsumer service

--- Backend/ImageHandlerMs/src/main/kotlin/com/backend/imagehandlerms/services/PythonMLConsumerService.kt
+++ Backend/ImageHandlerMs/src/main/kotlin/com/backend/imagehandlerms/services/PythonMLConsumerService.kt
@@ -1,18 +1,18 @@
-package com.backend.imagehandlerms.services
-
-import org.springframework.amqp.rabbit.annotation.RabbitListener
-import org.springframework.stereotype.Service
-
-@Service
-class PythonMLConsumerService {
-
-    @RabbitListener(queues = ["\${RABBITMQ_QUEUE}"])
-    fun receiveMessage(message: String) {
-        println("Received message: $message")
-    }
-
-    @RabbitListener(queues = ["\${PYTHON_SERVICE_QUEUE}"])
-    fun receivePythonServiceMessage(message: String) {
-        println("Received message from Python service: $message")
-    }
-}
+//package com.backend.imagehandlerms.services
+//
+//import org.springframework.amqp.rabbit.annotation.RabbitListener
+//import org.springframework.stereotype.Service
+//
+//@Service
+//class PythonMLConsumerService {
+//
+//    @RabbitListener(queues = ["\${RABBITMQ_QUEUE}"])
+//    fun receiveMessage(message: String) {
+//        println("Received message: $message")
+//    }
+//
+//    @RabbitListener(queues = ["\${PYTHON_SERVICE_QUEUE}"])
+//    fun receivePythonServiceMessage(message: String) {
+//        println("Received message from Python service: $message")
+//    }
+//}
