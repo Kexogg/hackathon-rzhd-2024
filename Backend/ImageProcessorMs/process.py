284108cbf92f90a5d2d92e1ab0ad62b1ba28f7cf
seaz96 <seaz442@mail.ru>
2024-05-18 18:09:09 +0500

    refactor + optimize algo

--- Backend/ImageProcessorMs/process.py
+++ Backend/ImageProcessorMs/process.py
@@ -1,48 +1,68 @@
 import cv2
 import contrast
 from shiftlab_ocr.doc2text.reader import Reader
+from shiftlab_ocr.doc2text.crop import Crop
+from PIL import Image
+
 import json
+
 
 def run(filename):
     reader = Reader()
     image = cv2.imread(filename)
+    contrast.normalize_contrast(image, filename)
+    crops = get_crops(filename, reader)
+    avg, dic = get_rows(crops, filename)
+    rs = process_rows(avg, crops, dic, reader)
+    text_result = get_rows_text(rs)
+    res = json.dumps(text_result)
+    return res
 
-    contrast_image = contrast.normalize_contrast(image, filename)
-    result = reader.doc2text(filename + ".png")
 
-    print(result[0])
-
-    summ = 0
-    count = len(result[1])
-    for i in result[1]:
-        summ += (i.points[1][1] - i.points[0][1])
-    avg = summ / count
-    print(avg)
-    dic = {}
-    img = cv2.imread('t.jpg.png')
-    for i in range(int(img.shape[1] / avg + 1)):
-        dic[i] = []
-
-    reader = Reader()
-
-    for i in result[1]:
-        middle = int((i.points[1][1] + i.points[0][1]) / 2)
-        text = reader.recognizer.run(i.img)
-        dic[int(middle / avg)].append((i, text))
-
-    rs = {}
-    for index, i in enumerate(dic.keys()):
-        if len(dic[i]) != 0:
-            rs[index] = dic[i]
-
+def get_rows_text(rs):
     text_result = {}
-
     for i in rs.keys():
         to_print = ''
         for word in rs[i]:
             to_print += word[1] + ' '
         text_result[i] = to_print
+    return text_result
 
-    res = json.dumps(text_result)
 
-    return res
+def process_rows(avg, crops, dic, reader):
+    for i in crops:
+        middle = int((i.points[1][1] + i.points[0][1]) / 2)
+        text = reader.recognizer.run(i.img)
+        dic[int(middle / avg)].append((i, text))
+    rs = {}
+    for index, i in enumerate(dic.keys()):
+        if len(dic[i]) != 0:
+            rs[index] = dic[i]
+    return rs
+
+
+def get_rows(crops, filename):
+    summ = 0
+    count = len(crops)
+    for i in crops:
+        summ += (i.points[1][1] - i.points[0][1])
+    avg = summ / count
+    print(avg)
+    dic = {}
+    img = cv2.imread(filename + ".png")
+    for i in range(int(img.shape[1] / avg + 1)):
+        dic[i] = []
+    return avg, dic
+
+
+def get_crops(filename, reader):
+    image = Image.open(filename + ".png")
+    boxes = reader.detector.run(filename + ".png")
+    crops = []
+    for box in boxes:
+        cropped = image.crop((box[0], box[1],
+                              box[2], box[3]))
+
+        crops.append(Crop([[box[0], box[1]], [box[2], box[3]]], img=cropped))
+    crops = sorted(crops)
+    return crops
