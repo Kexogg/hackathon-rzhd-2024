c99da06e46ec89081e00939855c210c325034709
Ilya <74655944+JabaXNT@users.noreply.github.com>
2024-05-18 11:41:32 +0500

    github actions added

--- Backend/ImageHandlerMs/src/main/kotlin/com/backend/imagehandlerms/services/PythonMLConsumerService.kt
+++ Backend/ImageHandlerMs/src/main/kotlin/com/backend/imagehandlerms/services/PythonMLConsumerService.kt
@@ -1,15 +1,20 @@
 package com.backend.imagehandlerms.services
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class RabbitMQConsumerService {
+class PythonMLConsumerService {
 
     @RabbitListener(queues = ["\${RABBITMQ_QUEUE}"])
     fun receiveMessage(message: String) {
         println("Received message: $message")
+        // process the message here
+    }
+
+    @RabbitListener(queues = ["\${PYTHON_SERVICE_QUEUE}"])
+    fun receivePythonServiceMessage(message: String) {
+        println("Received message from Python service: $message")
         // process the message here
     }
 }
